ba1614ddc182da33715e21ee061930e621e6d874
Ulaskhan <ulan0505ulan@gmail.com>
2024-01-28 01:16:36 +0600

    final

--- TSIS 1/ex1.py
+++ TSIS 1/ex1.py
@@ -1,4 +1,99 @@
 print("hello World")
-a = int(input())
-b = int(input())
-print(a +b - a *b)
+
+
+if(5>2):
+    print("Hello World")
+
+
+
+#This is a comment
+"""
+This is
+an other way
+to comment 
+"""
+carname = "Mercedes"
+x = 69
+
+x = 5
+y = 10
+print(x+y)
+
+x = 5
+y = 10
+z = x+y
+print(z)
+
+x,y,z = "Orange", "Banana", "Cherry"
+print(x , y , z)
+
+x= y = z = "Orange"
+print(x + y + z )
+def myfunc():
+    global xx
+    xx= 10
+myfunc()
+
+print(5 + xx)
+
+x = 5
+print(type(x))
+
+z = 1j
+print(type(z))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = float(1)   
+y = float(2.8)   
+z = float("3")   
+w = float("4.2") 
+
+    
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+b = "Hello, World!"
+print(b[2:5])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+txt = "We are the so-called \"Vikings\" from the north."
+
+
